30ba6979956038df71c4e2a331ab3417d6690de4
Rexe0 <tombring12@gmail.com>
2023-08-23 19:27:32 +1000

    Reduced Emerald Cost for special trader items

--- src/main/java/me/rexe0/bettersurvival/mobs/WanderingTrader.java
+++ src/main/java/me/rexe0/bettersurvival/mobs/WanderingTrader.java
@@ -14,7 +14,7 @@ public class WanderingTrader implements Listener {
     public void onSpawn(CreatureSpawnEvent e) {
         if (!(e.getEntity() instanceof org.bukkit.entity.WanderingTrader trader)) return;
         MerchantRecipe trade = new MerchantRecipe(RandomUtil.getRandom().nextBoolean() ? ItemType.STOPWATCH.getItem().getItem() : ItemType.METAL_DETECTOR.getItem().getItem(), 1);
-        trade.addIngredient(new ItemStack(Material.EMERALD, 30));
+        trade.addIngredient(new ItemStack(Material.EMERALD, 20));
         trade.addIngredient(new ItemStack(trade.getResult().getType(), 1));
         trader.setRecipe(trader.getRecipeCount()-2, trade);
     }
