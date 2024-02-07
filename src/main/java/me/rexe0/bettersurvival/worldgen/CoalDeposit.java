6497c27831f31e9a00ec98b3bf531834135364bb
Rexe0 <tombring12@gmail.com>
2024-02-07 21:32:05 +1000

    Added small surface exposures for coal ore

--- src/main/java/me/rexe0/bettersurvival/worldgen/CoalDeposit.java
+++ src/main/java/me/rexe0/bettersurvival/worldgen/CoalDeposit.java
@@ -22,7 +22,8 @@ public class CoalDeposit extends BlockPopulator {
                 int dx = x + chunkX * 16;
                 int dz = z + chunkZ * 16;
 
-                int y = limitedRegion.getHighestBlockYAt(dx, dz, HeightMap.OCEAN_FLOOR_WG)-6;
+                boolean exposed = random.nextInt(0, 50) == 0; // 1 in 50 chance for the ore to be exposed to the surface
+                int y = limitedRegion.getHighestBlockYAt(dx, dz, HeightMap.OCEAN_FLOOR_WG)- (exposed ? 2 : 6);
 
                 for (int i = -2; i < 3; i++) {
                     double noise = generator.noise(dx, dz, 0.01, 40, true);
@@ -35,12 +36,14 @@ public class CoalDeposit extends BlockPopulator {
                     if (!limitedRegion.getType(dx, y+i, dz).isSolid()) continue;
 
                     // Ensure that no adjacent blocks are exposed to air
-                    if (limitedRegion.getType(dx+1, y+i, dz) == Material.AIR
-                    || limitedRegion.getType(dx-1, y+i, dz) == Material.AIR
-                    || limitedRegion.getType(dx, y+i, dz+1) == Material.AIR
-                    || limitedRegion.getType(dx, y+i, dz-1) == Material.AIR) continue;
+                    if (!exposed)
+                        if (limitedRegion.getType(dx+1, y+i, dz) == Material.AIR
+                        || limitedRegion.getType(dx-1, y+i, dz) == Material.AIR
+                        || limitedRegion.getType(dx, y+i, dz+1) == Material.AIR
+                        || limitedRegion.getType(dx, y+i, dz-1) == Material.AIR) continue;
 
-                    limitedRegion.setType(dx, y+i, dz, Material.COAL_ORE);
+                    // TODO: TEST EXPOSED
+                    limitedRegion.setType(dx, y+i, dz, Material.OAK_FENCE);
                 }
 
             }
