e54732dc0f3a4bc17444745d88f1d6ec11e04db8
Rexe0 <tombring12@gmail.com>
2024-02-24 14:44:54 +1000

    Fixed issue with skull textures

--- src/main/java/me/rexe0/bettersurvival/util/SkullUtil.java
+++ src/main/java/me/rexe0/bettersurvival/util/SkullUtil.java
@@ -2,11 +2,12 @@ package me.rexe0.bettersurvival.util;
 
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.properties.Property;
-import com.mojang.authlib.properties.PropertyMap;
 import org.apache.commons.codec.binary.Base64;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.UUID;
 
 public class SkullUtil {
@@ -18,16 +19,21 @@ public class SkullUtil {
      * @return itemstack
      */
     public static ItemStack getCustomSkull(ItemStack head, String url) {
-        GameProfile profile = new GameProfile(UUID.randomUUID(), null);
-        PropertyMap propertyMap = profile.getProperties();
-        if (propertyMap == null) {
-            throw new IllegalStateException("Profile doesn't contain a property map");
+
+        SkullMeta headMeta = (SkullMeta) head.getItemMeta();
+        GameProfile profile = new GameProfile(UUID.randomUUID(), UUID.randomUUID().toString());
+        byte[] encodedData = Base64.encodeBase64(String.format("{textures:{SKIN:{url:\"%s\"}}}", url).getBytes());
+        profile.getProperties().put("textures", new Property("textures", new String(encodedData)));
+        Method setProfileMethod = null;
+        try {
+            setProfileMethod = headMeta.getClass().getDeclaredMethod("setProfile", GameProfile.class);
+            setProfileMethod.setAccessible(true);
+            setProfileMethod.invoke(headMeta, profile);
+        } catch (NoSuchMethodException | IllegalArgumentException | IllegalAccessException |
+                 InvocationTargetException e1) {
+            e1.printStackTrace();
         }
-        byte[] encodedData = base64.encode(String.format("{textures:{SKIN:{url:\"%s\"}}}", url).getBytes());
-        propertyMap.put("textures", new Property("textures", new String(encodedData)));
-        ItemMeta headMeta = head.getItemMeta();
-        Class<?> headMetaClass = headMeta.getClass();
-        Reflections.getField(headMetaClass, "profile", GameProfile.class).set(headMeta, profile);
+
         head.setItemMeta(headMeta);
         return head;
     }
